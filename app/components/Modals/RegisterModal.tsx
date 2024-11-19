0cd7ab0da9457a0a05850006ab7030c1728a1689
jannatulFerdeous <angeljannat673@gmail.com>
2024-11-19 17:38:12 +0600

    Created the user register form

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modals from './Modals';
 import Heading from '../Heading';
+import Input from '../inputs/Input';
 const RegisterModal = () => {
   const registerModel = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
@@ -18,8 +19,8 @@ const RegisterModal = () => {
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
-      name: ' ',
-      email: ' ',
+      name: '',
+      email: '',
       password: '',
     },
   });
@@ -42,6 +43,31 @@ const RegisterModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </div>
   );
 
