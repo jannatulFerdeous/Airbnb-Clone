11a345ddd7bca91e71fb7e26f0cc2b76526c9108
jannatulFerdeous <angeljannat673@gmail.com>
2024-12-01 15:53:50 +0600

    Upadated the database

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -37,7 +37,7 @@ const RegisterModal = () => {
         registerModel.onClose();
       })
       .catch(error => {
-        toast.error('something went wrong');
+        toast.error('something went wrong', error);
       })
       .finally(() => {
         setIsLoading(false);
