8373a289a0880f51342186350a6b2f06e19d84bc
jannatulFerdeous <angeljannat673@gmail.com>
2025-01-15 17:32:02 +0600

    add CountrySelect component and integrate it into RentModal for improved location selection

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -8,6 +8,7 @@ import Heading from '../Heading';
 import { categoriesData } from '../Navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
+import CountrySelect from '../inputs/CountrySelect';
 
 enum STEPS {
   CATEGORY = 0,
@@ -111,6 +112,7 @@ const RentModal = () => {
           title="Where is your place located?"
           subtitle="Help guests find you!"
         />
+        <CountrySelect />
       </div>
     );
   }
--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,5 +1,56 @@
-const CountrySelect = () => {
-  return <div>CountrySelect</div>;
+'use client';
+
+import useCountries from '@/app/hooks/useCountries';
+import Select from 'react-select';
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlag: number[];
+  region: string;
+  value: string;
 };
 
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange: (value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
+  const { getAll } = useCountries();
+  return (
+    <div>
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={getAll()}
+        value={value}
+        onChange={value => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className="flex flex-row items-center gap-3">
+            <img src={option.flag} alt={option.label} className="w-6 h-4" />
+            <div>
+              {option.label}
+              <span className="text-neutral-500 ml-1"> {option.region}</span>
+            </div>
+          </div>
+        )}
+        classNames={{
+          control: () => 'p-3 border-2',
+          input: () => 'text-lg',
+          option: () => 'text-lg',
+        }}
+        theme={theme => ({
+          ...theme,
+          borderRadius: 6,
+          colors: {
+            ...theme.colors,
+            primary: 'black',
+            primary25: '#ffe4e6',
+          },
+        })}
+      ></Select>
+    </div>
+  );
+};
 export default CountrySelect;
